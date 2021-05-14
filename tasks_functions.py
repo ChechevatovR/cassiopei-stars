fea83dc68c2208296bdcf29e9228d09cfe4e18e2
Chechevatov <chechevatov.r@gmail.com>
2021-05-14 19:48:48 +0700

    Починил задачу Цветовод Теперь ответ просит в HEX и принимает когда он правильный

--- tasks_functions.py
+++ tasks_functions.py
@@ -230,12 +230,13 @@ def task23_solve(text: str) -> str:
 
 def task29_generator(team_id: int):
     filepath = f'task-generated-content/29/task29_{team_id}.png'
+
     colour = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
     img = Image.new("RGB", (2, 2), colour)
     img.save(filepath, "PNG")
-
-    req_colour = (255 - colour[0], 255 - colour[1], 255 - colour[2])
-    return f'<img src=/{filepath} width=100%>', str(req_colour)
+    answer = '#' + ''.join(map(lambda colour: hex(255 - colour)[2:].upper().zfill(2), colour))
+    print(colour, answer)
+    return f'<img src=/{filepath} width=100%>', answer
 
 
 def task43_generator(team_id: int):
